ef6e5e55d8f680cb9c9cce20eb8725bb5bbf5af9
HariPrasanth-cse <140779558+HariPrasanth-cse@users.noreply.github.com>
2023-07-31 15:35:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Demo{
+methodone()
+{
+    var a=5;
+    var b=10;
+    var c=a+b;
+    document.getElementById('root').innerHTML=c;
+}
+}
+var mydemo = new Demo();
+mydemo.methodone();
+
+import React, {Component} from "react";
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      data:
+    [
+      {
+        "name":"Fazil : Bai"
+      },
+      {
+        "name":"Bharath : Gowda"
+      },
+      {
+        "name":"Bala : Smile face "
+      },
+      {
+        "name":"Chandru : Gym Body"
+      },
+      {
+        "name":"Anantha Krishnan : Old Joke"
+      },
+      {
+        "name":"Selva : Sound"
+      }
+    ]
+    }
+  }
+
+render(){
+  return(
+    <div>
+      <Movie/>
+      <ul>
+        {this.state.data.map((item)=><List data={item}/>)}
+      </ul>
+    </div>
+  );
+}
+}
+class Movie extends React.Component{
+render(){
+  return(
+    <div>
+      <h1 style={{color:'#2CF104', textAlign: "center"}}>Buddys Club</h1>
+      <h2 style={{color:'#04F1ED', textAlign: "center"}}>Its about My Friends </h2>
+      {/* <h1 style={{ color: '#8504F1', textAlign: "center" }}>TIME TO TRAVEL</h1><h2>LADAKH</h2> */}
+    </div>
+  );
+}
+}
+class List extends React.Component{
+  render(){
+    return(
+      <ul>
+        <li>{this.props.data.name}</li>
+      </ul>
+      
+    );
+  }
+}
+export default App;
